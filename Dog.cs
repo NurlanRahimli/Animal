4148a2c1fcd16035a019f34fad15ce5a984ca226
NurlanRahimli <nurlanshr@code.edu.az>
2022-07-07 11:15:12 +0400

    Dog.cs

---
+++ Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tezetema
+{
+    class Dog : Animal
+    {
+        public string name;
+        public string color;
+        public byte age;
+
+        public Dog(string n, string c, byte a)
+        {
+            name = n;
+            color = c;
+            age = a;
+
+            Console.WriteLine("Custom construktor worked");
+        }
+
+        public string GetDetails()
+        {
+            return $"{name} {color} {age}";
+        }
+    }
+}
