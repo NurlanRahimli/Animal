407e3b9c5016928c02c0df88007e69c0b812d1b0
NurlanRahimli <nurlanshr@code.edu.az>
2022-07-07 11:16:41 +0400

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tezetema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Kotik", "black", 7);
+            Dog dog = new Dog("Cek", "white", 3);
+
+            Console.WriteLine(cat.GetDetails());
+            Console.WriteLine(dog.GetDetails());
+
+        }
+    }
+}
