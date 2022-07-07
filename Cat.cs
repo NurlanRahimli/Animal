f8b8d4dc94597c110707c873e18c67c6e437a3c8
NurlanRahimli <nurlanshr@code.edu.az>
2022-07-07 11:14:37 +0400

    Cat.cs

---
+++ Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace tezetema
+{
+    public class Cat 
+    {
+        public string name;
+        public string color;
+        public byte age;
+
+        public Cat(string n, string c, byte a)
+        {
+            name = n;
+            color = c;
+            age = a;
+
+            Console.WriteLine("Custom construktor worked");
+        }
+
+
+        public string GetDetails()
+        {
+            return $"{name} {color} {age}";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
