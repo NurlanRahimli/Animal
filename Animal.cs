66a83ec35d9876f37cc34672e5730b566d9ab4f6
NurlanRahimli <nurlanshr@code.edu.az>
2022-07-07 11:13:35 +0400

    Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tezetema
+{
+    class Animal
+    {
+        
+            public string name;
+            public string color;
+            public byte age;
+
+            public Animal()
+            {
+
+            }
+
+            public Animal(string n, string c, byte a)
+            {
+                name = n;
+                color = c;
+                age = a;
+
+                Console.WriteLine("Custom construktor worked");
+            }
+
+
+            public string GetDetails()
+            {
+                return $"{name} {color} {age}";
+            }
+        
+    }
+}
